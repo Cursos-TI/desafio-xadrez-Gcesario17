13cd1e03d649ecc745c581c93ac955c2bd37fc8b
Gcesario17 <154361697+Gcesario17@users.noreply.github.com>
2025-10-30 15:45:08 -0300

    nessa atualização do desafio para aventureiro, usamos os loops aninhados onde o FOR era obrigatorio, e eu usei o while como uma segunda opção, tive uma dificuldade de fazer rodar ele aninhado onde tive q fazer uma pesquisa de como eu poderia solucionar o problema de copilar 2x o movimento esquerda, achei a solução onde eu usei && para solucionar este problema, legal passar essas dificuldades e pooder aprender as formas de resolve=las

--- xadrez.c
+++ xadrez.c
@@ -2,8 +2,9 @@
 
 /*
   Programa: Movimentando as Peças do Xadrez
-  Objetivo: Simular os movimentos da Torre, Bispo e Rainha usando for, while e do-while
-  NIVEL NOVATO
+  Objetivo NIvel NOvato: Simular os movimentos da Torre, Bispo e Rainha usando for, while e do-while
+  Obejetivo NIvel Aventureiro simular o movimento do cavalo em L, usando estruturas aninhadas com 2 loops diferente, sendo um deles for e outro while
+  NIVEL Aventureiro
   Autor: Gabriel de Oliveira Cesário
 */
 
@@ -50,17 +51,32 @@ int main() {
 
    } while(j <= CasasRainha);
     
-   // Finalização do Programa de Nivel Novato MOvimento de Peças de Xadrez
-
-    printf("\n*** Movimentação concluída com sucesso! ***\n"); 
-    
-    
-    
-    
-    
 // Nível Aventureiro - Movimentação do Cavalo
-// Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-// Um loop pode representar a movimentação horizontal e outro vertical.
+// Vamos utilizar loops aninhados para simular a movimentação do Cavalo em L, usando 2 loops diferentes, tendo obrigatoriedade de usar o FOR
+
+   int MovBaixo = 2;
+   int MovEsquerda = 1;
+   int passo1, passo2;
+
+   printf ("\n*** Movimento Cavalo ***\n"); //Entrada de Movimento Cavalo o Movimento sera em L, onde Vai ser impresso Baixo,Baixo feito por for e Esquerda feito por while, o for é obrigatorio no desafio
+
+   for(passo1 = 1; passo1 <= MovBaixo; passo1++) //1º loop, usei o FOR que era obrigatorio, e como é ensinado na aula, ele geralmente é o mais usado em uma estrutura aninhada
+   {
+    printf ("Casa %d: Baixo! \n", passo1); //1º impressao de movimento saira Baixo e Baixo
+
+   passo2 = 1; //declaração de valor de variavel
+
+   while(passo2 <= MovEsquerda && passo1 == MovBaixo) /*2º loop, usei o while, eu tive que usar &&, pois quando eu rodava saia como baio, esquerda,baixo esquerda
+                                                        ou seja errado, usando esse && anulou esse esquerda a mais no programa
+                                                        sinceramente tive que procurar como eu fazia isso rodar de uma forma aninhada corretamente*/
+   {
+    printf ("Casa %d: Esquerda! \n", passo2); //2º impressao de movimento saira Esquerda
+    passo2++;
+   }   }
+
+     printf("\n*** Movimento de Xadrez Nivel Aventureiro Concluído com Sucesso! ***\n");
+
+
 
 // Nível Mestre - Funções Recursivas e Loops Aninhados
 // Sugestão: Substitua as movimentações das peças por funções recursivas.
